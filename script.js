99783a4d2c95d81d3b6f2eaf1084467fa3556dec
Dverybest <duio1541@gmail.com>
2024-03-09 20:58:41 -0500

    renaming the alert

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-alert("Awesome calculator")
+alert("Calculate with Ease")
 let calc = "";
 document.getElementById("display").innerHTML = calc;
 
 
-// document.getElementById("resetButton").addEventListener("click", resetCalculator);
-// function resetCalculator() {
-//     // Set relevant variables or states to zero or initial values
-//     document.getElementById("display").innerHTML = ""; // Reset the displayed result
-//     // You may need to reset other variables or states here depending on your implementation
-// }
